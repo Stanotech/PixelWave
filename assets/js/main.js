a4080c33651ce08ea05ee9dff7e45c3767581cf6
Sta Rog <satach@o2.pl>
2024-05-20 13:21:55 +0200

    google analitics, robots.txt, and other seo

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,16 @@
 (function() {
   "use strict";
 
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/662c9ab1a0c6737bd1315f1b/1hsf3oi4e';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+  })();
+  
   /**
    * Easy selector helper function
    */
@@ -374,5 +384,6 @@
     link.href = '/assets/build/vendors.min.css';
     document.head.appendChild(link);
   };
+  
   
 })()
